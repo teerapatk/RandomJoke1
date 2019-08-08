cec18b8262dd348fabb555c30877ad58a8c7e44a
Teerapat <teerapatkij1412@gmail.com>
2019-08-08 14:11:42 +0700

    Initial Commit

---
+++ RandomJoke1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJoke1
+//
+//  Created by Teerapat on 8/8/2562 BE.
+//  Copyright © 2562 Teerapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
